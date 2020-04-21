0a6d58439641b3ae510ba77132e174234b4daf33
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-04-21 18:12:25 +0530

    Initial Commit

--- mongoose.js
+++ mongoose.js
@@ -6,3 +6,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/test-api",
     useCreateIndex:true,
     useUnifiedTopology:true
 })
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ArjunBajpai:IAmArjun_Goa16@cluster0-5gpft.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
