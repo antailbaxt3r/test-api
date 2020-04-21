8bd776b047a4e06df24fa7cf43fb7bbf03be2ab8
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-04-21 18:12:50 +0530

    Initial Commit

--- mongoose.js
+++ mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/test-api", 
-{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology:true
-})
+// mongoose.connect("mongodb://127.0.0.1:27017/test-api", 
+// {
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     useUnifiedTopology:true
+// })
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://ArjunBajpai:IAmArjun_Goa16@cluster0-5gpft.mongodb.net/test?retryWrites=true&w=majority";
