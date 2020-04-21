b6b7f23f16f55d7ffd6ea042633f9090ec56d442
antailbaxt3r <arjunbajpaiwork@gmail.com>
2020-04-21 18:25:51 +0530

    Fixed URI

--- mongoose.js
+++ mongoose.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect("mongodb://127.0.0.1:27017/test-api", 
-// {
-//     useNewUrlParser:true,
-//     useCreateIndex:true,
-//     useUnifiedTopology:true
-// })
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test-api", 
+{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true
+})
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ArjunBajpai:IAmArjun_Goa16@cluster0-5gpft.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test-api").collection("Person");
-  // perform actions on the collection object
-  client.close();
-});
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://ArjunBajpai:IAmArjun_Goa16@cluster0-5gpft.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test-api").collection("Person");
+//   // perform actions on the collection object
+//   client.close();
+// });
